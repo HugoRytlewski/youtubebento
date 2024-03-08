3b65e7e7b418718d1d80f0589e44ef6b40a1e89f
Rytlewski Hugo <hrytlewski@nexton-group.com>
2024-03-08 15:39:14 +0100

    test prod

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,23 +2,19 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
-
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-
   image: {
     dir: "assets/img",
   },
-
   imports: {
     dirs: [
       "constants",
     ],
   },
-
   modules: ["@nuxt/image"]
 })
