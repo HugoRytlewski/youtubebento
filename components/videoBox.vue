730cc2f0e41b75fc12c96f8b7f546dd40041a126
Hugo <rytlewski.hugo@etu.educ-valadon-limoges.fr>
2024-03-11 15:29:46 +0100

    fix picture

--- components/videoBox.vue
+++ components/videoBox.vue
@@ -16,6 +16,9 @@ const {
   date_sortie: String,
   temps: String,
 });
+
+const fixMiniature = "/image/" + miniature;
+const fixLogo = "/image/" + logo_chaine;
 </script>
 
 <template>
@@ -23,13 +26,14 @@ const {
     <div
       class="relative w-[292px] ml-[24px] rounded-xl h-[250px] overflow-hidden z-50 group-hover:scale-105 duration-200 delay-100"
     >
-      <div class="h-[164px] overflow-hidden relative ">
+      <div class="h-[164px] overflow-hidden relative">
         <img
           class="z-50 absolute"
-          :src="`/_nuxt/assets/img/${miniature}`"
+          :src="fixMiniature"
           height="164"
           alt="Image Description"
         />
+
         <div
           class="z-50 absolute bottom-2 right-2 p-2 h-[16px] text-white bg-black bg-opacity-75 rounded-full text-center flex items-center justify-center group-hover:opacity-0 duration-200"
         >
@@ -41,7 +45,7 @@ const {
         <div class="relative w-72 h-40 shadow-2xl">
           <img
             class="w-full h-full object-cover filter blur-xl scale-150"
-            :src="`/_nuxt/assets/img/${miniature}`"
+            :src="fixMiniature"
           />
           <div
             class="absolute top-0 h-full w-[110%] bg-gray-500 opacity-50"
@@ -51,10 +55,8 @@ const {
         <div
           class="absolute bottom-0 h-[164px] bg-gradient-to-t from-black w-[110%] p-[12px] flex gap-[8px]"
         >
-          <div
-            class="absolute w-[28px] h-[28px] rounded-lg overflow-hidden"
-          >
-            <img :src="`/_nuxt/assets/img/${logo_chaine}`" alt="" />
+          <div class="absolute w-[28px] h-[28px] rounded-lg overflow-hidden">
+            <img :src="fixLogo" alt="" />
           </div>
           <div class="flex flex-col">
             <p class="ml-[40px] text-white text-[14px] w-[14rem] line-clamp-2">
@@ -69,7 +71,7 @@ const {
     </div>
     <img
       class="w-[9rem] h-[9rem] object-cover absolute filter blur-2xl opacity-20 scale-[2.1] hidden group-hover:flex duration-300"
-      :src="`/_nuxt/assets/img/${miniature}`"
+      :src="fixMiniature"
     />
   </div>
 </template>
