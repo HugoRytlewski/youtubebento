d6b2f064fc729888e2dd5e53179f8e1ffda42a89
Hugo <rytlewski.hugo@etu.educ-valadon-limoges.fr>
2024-04-03 12:36:25 +0200

    fix href footer

--- components/footerDesign.vue
+++ components/footerDesign.vue
@@ -2,7 +2,7 @@
 <template> 
     <footer class="max-w-screen bg-gray-700 bg-opacity-40 h-20 items-center p-4 rounded-lg m-4 mb-96 md:w-[78rem] h-16 flex ">
         <div class="w-screen-xl items-center  flex flex-col it md:flex-row md:justify-between text-gray-400 ">
-            <p>Design de Mickadoule ,  <a class="hover:underline" href="https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbk5IZi1qNHlXVlZCU0J3alRBSU11SmVyek1UUXxBQ3Jtc0tsZnJRNXk4SHFwSTNPZHNlZVF6ZnV5OGloRHZ0STNETUxrb1J5NlJCMUZ1R1RCNmxweDNXemkyWG5RRVFMWjItRW5XUkpmcUhfQllxbU1tcEJmLWc2M2VvSGVoUGZJT25LUmRlbE5Bd1FqQWwxODFJOA&q=https%3A%2F%2Fwww.figma.com%2Fcommunity%2Ffile%2F1314291184219843263%2Fyoutube-bento-ui&v=dscm2mvfeYs">voir Design Ici</a></p>  
+            <p>Design de Mickadoule ,  <a class="hover:underline" target="_blank" href="https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbk5IZi1qNHlXVlZCU0J3alRBSU11SmVyek1UUXxBQ3Jtc0tsZnJRNXk4SHFwSTNPZHNlZVF6ZnV5OGloRHZ0STNETUxrb1J5NlJCMUZ1R1RCNmxweDNXemkyWG5RRVFMWjItRW5XUkpmcUhfQllxbU1tcEJmLWc2M2VvSGVoUGZJT25LUmRlbE5Bd1FqQWwxODFJOA&q=https%3A%2F%2Fwww.figma.com%2Fcommunity%2Ffile%2F1314291184219843263%2Fyoutube-bento-ui&v=dscm2mvfeYs">voir Design Ici</a></p>  
         </div>
     </footer>
 </template>
