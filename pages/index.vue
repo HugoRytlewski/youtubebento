b070f3f19cfbec2834dbc475020f330716e91af1
Hugo <rytlewski.hugo@etu.educ-valadon-limoges.fr>
2024-04-04 21:53:58 +0200

    fix btn

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,6 @@ definePageMeta({
 <template>
   <div class="p-6 h-screen w-screen flex flex-col gap-10 items-center justify-center">
     <p class="text-white font-bold text-2xl ">Ce projet est en cours de développement , certaines fonctionnalités ne sont donc pas opérationnelles </p>
-      <a href="/accueil" class="h-14 bg-red-600 hover:bg-red-800 duration-200 text-white font-bold rounded-xl p-2 ">Accéder à la page d'accueil</a>
+      <a href="/accueil" class="h-14 bg-red-600 hover:bg-red-800 duration-200 text-white flex items-center justify-center font-bold rounded-xl p-2 ">Accéder à la page d'accueil</a>
   </div>
 </template>
